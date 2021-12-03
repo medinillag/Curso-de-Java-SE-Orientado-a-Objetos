632ac04b2c302e436bb076de1e8a83c446a24a36
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-12-03 11:16:10 -0600

    Método constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,14 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor() {
+        System.out.println("Construyendo el objeto doctor");
+    }
+
+    Doctor(String name) {
+        System.out.println("El nombre del doctor asignado es: " + name);
+    }
+
     //Comportamientos
     public void showName() {
         System.out.println(name);
