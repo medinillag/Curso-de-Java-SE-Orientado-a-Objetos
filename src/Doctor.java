650e34c92b06d8f07bd739b77caeaa631ed0500c
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2021-12-03 11:29:05 -0600

    estático

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,10 +1,12 @@
 public class Doctor {
-    int id;
+    //Atributos
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor() {
         System.out.println("Construyendo el objeto doctor");
+        id++;
     }
 
     Doctor(String name) {
@@ -14,5 +16,9 @@ public class Doctor {
     //Comportamientos
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-    Doctor myDoctor = new Doctor();
-    myDoctor = new Doctor("Anahí Salgado");
-    myDoctor.name = "Alejandro Rodríguez";
-    myDoctor.showName();
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Alejandro Rodríguez";
+        myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor.id++;
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
     }
 }
